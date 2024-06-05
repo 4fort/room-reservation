7e9fc5f55ed8990744d7b8de9f0a9857d7f2ddef
Dexter Fort <dexterkun16@gmail.com>
2024-06-05 15:58:22 +0800

    getRooms function limit argument

--- src/app/_home/_components/available-rooms.tsx
+++ src/app/_home/_components/available-rooms.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function AvailableRooms() {
-  const rooms = await api.rooms.getHomepage();
+  const rooms = await api.rooms.getAll({ limit: 3 });
   return (
     <React.Fragment>
       <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
